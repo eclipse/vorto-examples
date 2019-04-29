010a903eea6fa3e6712280290d6ffbd1a6d0dc8e
vlvanchin <vlvanchin@gmail.com>
2019-04-29 22:35:08 +0800

    works for master and script file renamed
    Signed-off-by: vlvanchin <vlvanchin@gmail.com>

--- vorto-generators/scripts/deploy_jar_with_images.sh
+++ vorto-generators/scripts/deploy_jar_with_images.sh
@@ -12,8 +12,11 @@ jar -cvf generator-runner-3rd-party-exec-withimg.jar .
 cp generator-runner-3rd-party-exec-withimg.jar ../example-generator-exec_${ELASTIC_BEANSTALK_LABEL}.jar
 cd ../..
 pwd
+# to upload the jar to s3
 aws s3 cp ./aws-upload/example-generator-exec_${ELASTIC_BEANSTALK_LABEL}.jar s3://$VORTO_S3_BUCKET --acl "private" --storage-class "STANDARD_IA" --only-show-errors --no-guess-mime-type
 sleep 60
+# to version the jar to EBS and refer to s3
 aws elasticbeanstalk create-application-version --application-name "VortoRepoServer" --no-auto-create-application --version-label "build-job_${ELASTIC_BEANSTALK_LABEL}_3rdparty" --description "Build ${TRAVIS_JOB_NUMBER} - Git Revision ${TRAVIS_COMMIT_SHORT}" --source-bundle S3Bucket="$VORTO_S3_BUCKET",S3Key="example-generator-exec_${ELASTIC_BEANSTALK_LABEL}.jar"
 sleep 60
+# to update env of EBS for application
 aws elasticbeanstalk update-environment --application-name "VortoRepoServer" --environment-name "Vortoexamplegenerators-env-dev" --version-label "build-job_${ELASTIC_BEANSTALK_LABEL}_3rdparty"
