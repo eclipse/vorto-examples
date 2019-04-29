370ba527e727a54cc8ce6adc97b2d0d474470603
vlvanchin <vlvanchin@gmail.com>
2019-04-29 21:26:28 +0800

    fix branch and copy from s3
    Signed-off-by: vlvanchin <vlvanchin@gmail.com>

--- vorto-generators/scripts/create_jar_with_images.sh
+++ vorto-generators/scripts/create_jar_with_images.sh
@@ -7,7 +7,7 @@ cp ./generator-runner/target/generator-runner-3rd-party-exec.jar ./aws-upload/tm
 cd aws-upload/tmp
 jar -xvf generator-runner-3rd-party-exec.jar
 rm -f generator-runner-3rd-party-exec.jar
-aws s3 cp s3://$VORTO_S3_BUCKET/img/ ./BOOT-INF/classes/img/
+aws s3 cp s3://$VORTO_S3_BUCKET/img ./BOOT-INF/classes/img --recursive
 jar -cvf generator-runner-3rd-party-exec-withimg.jar .
 cp generator-runner-3rd-party-exec-withimg.jar ../example-generator-exec_${ELASTIC_BEANSTALK_LABEL}.jar
 cd ../..
