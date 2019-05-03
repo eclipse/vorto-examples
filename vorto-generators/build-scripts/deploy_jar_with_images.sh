e645d7217840e3e98a0b85fadff2e826de4f2588
Lokanathan Vanchinathan <vanchinathan.lokanathan@bosch-si.com>
2019-05-03 20:00:37 +0800

    Feature/issue test travisci with var substitution (#46)
    * added variable substitution
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * rightly positioned install process
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * Fixes #34, awscli is installed once and variable substitution is done
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * proper labeling done
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * proper labeling done
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * switch to work on master branch
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * adding 3rd party images to jar
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * generators images are reference to the corresponding images
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * fix mkdir
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * fix folder path
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * added comments and keys
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * use script to do the tasks of putting the images
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * updated script path
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * display pwd
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * adding to script file
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * added bash scritp
    
    Signed-off-by: vlvanchin <vlvanchin@gmail.com>
    
    * included the pwd
    
    Signed-off-by: vlvanchin <vlvanchin@gmail.com>
    
    * fix branch and copy from s3
    
    Signed-off-by: vlvanchin <vlvanchin@gmail.com>
    
    * update script to upload to s3
    
    Signed-off-by: vlvanchin <vlvanchin@gmail.com>
    
    * to move from .yml to bash script
    
    Signed-off-by: vlvanchin <vlvanchin@gmail.com>
    
    * moving code to bash script
    
    Signed-off-by: vlvanchin <vlvanchin@gmail.com>
    
    * works for master and script file renamed
    
    Signed-off-by: vlvanchin <vlvanchin@gmail.com>
    
    * adding the same kind of MANIFEST.MF file
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * pulling images from s3 img/examples folder
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * configure to do proper jaring with images
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * deploys for master branch alone
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * corrected image reference
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>

---
+++ vorto-generators/build-scripts/deploy_jar_with_images.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+#
+# to create the jar with images
+#
+mkdir -p aws-upload/tmp
+cp ./generator-runner/target/generator-runner-3rd-party-exec.jar ./aws-upload/tmp/
+cd aws-upload/tmp
+jar -xvf generator-runner-3rd-party-exec.jar
+rm -f generator-runner-3rd-party-exec.jar
+aws s3 cp s3://$VORTO_S3_BUCKET/img/examples ./BOOT-INF/classes/img --recursive
+jar -cvmf0 META-INF/MANIFEST.MF generator-runner-3rd-party-exec-withimg.jar .
+cp generator-runner-3rd-party-exec-withimg.jar ../example-generator-exec_${ELASTIC_BEANSTALK_LABEL}.jar
+cd ../..
+pwd
+
+# to upload the jar to s3
+echo "uploading to s3"
+aws s3 cp ./aws-upload/example-generator-exec_${ELASTIC_BEANSTALK_LABEL}.jar s3://$VORTO_S3_BUCKET --acl "private" --storage-class "STANDARD_IA" --only-show-errors --no-guess-mime-type
+sleep 60
+
+# to version the jar to EBS and refer to s3
+echo "setting versioning in EBS"
+aws elasticbeanstalk create-application-version --application-name "VortoRepoServer" --no-auto-create-application --version-label "build-job_${ELASTIC_BEANSTALK_LABEL}_3rdparty" --description "Build ${TRAVIS_JOB_NUMBER} - Git Revision ${TRAVIS_COMMIT_SHORT}" --source-bundle S3Bucket="$VORTO_S3_BUCKET",S3Key="example-generator-exec_${ELASTIC_BEANSTALK_LABEL}.jar"
+sleep 60
+
+# to update env of EBS for application
+echo "updating environment in EBS"
+aws elasticbeanstalk update-environment --application-name "VortoRepoServer" --environment-name "Vortoexamplegenerators-env-dev" --version-label "build-job_${ELASTIC_BEANSTALK_LABEL}_3rdparty"
