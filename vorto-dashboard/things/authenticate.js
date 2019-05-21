8c03369a4a9b8f082e22bf8e3b554b1ab250677b
Tim Großmann <contact.timgrossmann@gmail.com>
2019-05-21 16:30:34 +0800

    prepare for publish to npm (#51)
    Signed-off-by: Tim Grossmann <contact.timgrossmann@gmail.com>

--- vorto-dashboard/things/authenticate.js
+++ vorto-dashboard/things/authenticate.js
@@ -1,5 +1,33 @@
 const request = require("request-promise-native");
-const config = require("../config.json")
+const path = require('path');
+const ArgumentParser = require("argparse").ArgumentParser;
+const parser = new ArgumentParser({
+  addHelp: true,
+  description: 'Eclipse Vorto Dashboard'
+});
+
+parser.addArgument(
+  ["-c", "-C", "--config"],
+  {
+    help: "Relative or absolute path to config file with OAuth2 Client credentials"
+  }
+);
+
+const cliArgs = process.argv.slice(2);
+let configFilePath
+
+// handle case of user only typing "vorto-dashboard config.json"
+if (cliArgs.length > 1 || cliArgs[0].startsWith("-")) {
+  const args = parser.parseArgs();
+  configFilePath = args.config
+} else {
+  configFilePath = cliArgs[0]
+}
+
+// check path to config file, if absolute, keep it, otherwise use working dir and create abs path
+const configPath = path.isAbsolute(configFilePath) ? configFilePath : path.join(process.cwd(), configFilePath);
+const configFile = require(configPath);
+console.log("Using config.json file from path: ", configPath)
 
 class AuthToken {
   /*Intitally get the token and assign its value to the object*/
@@ -11,9 +39,9 @@ class AuthToken {
   getInitialToken() {
     const tokenForm = {
       "grant_type": "client_credentials",
-      "client_id": config.bosch_iot_suite.client_id,
-      "client_secret": config.bosch_iot_suite.client_secret,
-      "scope:service": config.bosch_iot_suite.scope
+      "client_id": configFile.client_id,
+      "client_secret": configFile.client_secret,
+      "scope:service": configFile.scope
     }
 
     return request(this.getReqOpts(tokenForm))
@@ -54,8 +82,8 @@ class AuthToken {
     console.log("Refreshing token")
     const tokenForm = {
       "grant_type": "refresh_token",
-      "client_id": config.bosch_iot_suite.client_id,
-      "client_secret": config.bosch_iot_suite.client_secret,
+      "client_id": configFile.client_id,
+      "client_secret": configFile.client_secret,
       "refresh_token": refreshToken
     }
 
