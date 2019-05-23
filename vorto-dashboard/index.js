c7c272b0516486b2663adb9ff691bede731de3ad
Tim Großmann <contact.timgrossmann@gmail.com>
2019-05-23 11:18:21 +0800

    Test release to NPM Travis
    Signed-off-by: Tim Grossmann <contact.timgrossmann@gmail.com>

--- vorto-dashboard/index.js
+++ vorto-dashboard/index.js
@@ -3,6 +3,8 @@
 const path = require('path');
 const express = require('express');
 
+console.log("Test")
+
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
