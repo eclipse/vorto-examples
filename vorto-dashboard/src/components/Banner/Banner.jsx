176c002ad39ce019959dc6fa7b4d956cc346a57c
Tim Grossmann <contact.timgrossmann@gmail.com>
2019-08-02 15:05:07 +0700

    add banner
    Signed-off-by: Tim Grossmann <contact.timgrossmann@gmail.com>

--- vorto-dashboard/src/components/Banner/Banner.jsx
+++ vorto-dashboard/src/components/Banner/Banner.jsx
@@ -3,12 +3,11 @@ import React, { Component } from "react";
 class Banner extends Component {
   render() {
     return (
-      <div />
-      /*<div className="banner">
-        <b>
-          <a href="https://www.npmjs.com/package/vorto-dashboard" target="_blank" rel="noopener noreferrer">Download from NPM</a>
-        </b>
-      </div>*/
+      <a href="https://www.npmjs.com/package/vorto-dashboard" target="_blank" rel="noopener noreferrer">
+        <div className="banner">
+          <b>Download from NPM</b>
+        </div>
+      </a>
     );
   }
 }
