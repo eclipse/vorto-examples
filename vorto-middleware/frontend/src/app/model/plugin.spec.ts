aeae213e0dd63e1d644f3d4f2913b476940b9628
Tim Tenckhoff <tmtnckff@icloud.com>
2019-10-22 19:28:55 +0800

    deleted folder
    Signed-off-by: Tim Tenckhoff <tmtnckff@icloud.com>

--- vorto-middleware/frontend/src/app/model/plugin.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Plugin } from './plugin';
-
-describe('Plugin', () => {
-  it('should create an instance', () => {
-    expect(new Plugin()).toBeTruthy();
-  });
-});
--- vorto-middleware/middleware-service/src/main/java/org/eclipse/vorto/middleware/monitoring/IPayloadMonitorCallback.java
+++
@@ -1,6 +0,0 @@
-package org.eclipse.vorto.middleware.monitoring;
-
-public interface IPayloadMonitorCallback {
-
-	void onMessage(MonitorMessage message);
-}
