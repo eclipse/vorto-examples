72dd8b4a85c941e7a7aa17b723b252b22657a510
tmtnckff <tmtnckff@icloud.com>
2019-10-23 13:55:56 +0800

    fixed gauges and percentage (#171)
    * added adaptions to gauge and percentage widget
    
    Signed-off-by: Tim Tenckhoff <tmtnckff@icloud.com>
    
    * fixed bug with empty percentage values
    
    Signed-off-by: Tim Tenckhoff <tmtnckff@icloud.com>

--- vorto-dashboard/src/views/Device/Cards/GaugeCard.jsx
+++ vorto-dashboard/src/views/Device/Cards/GaugeCard.jsx
@@ -7,12 +7,17 @@ export class GaugeCard extends Component {
         // TODO adjust retrieval of attributes to be smarter
         // (Get the definition of the function blocks and find attribute values)
         const values = this.props.feature.properties
+        var unit = (values.status.unit) ? values.status.unit : ""
+        if(unit === "second"){
+            unit += "s"
+        }
         const currVal = values.status.value.currentMeasured || 0
         const minVal = values.status.value.minMeasured || 0
         const maxVal = values.status.value.maxMeasured || currVal
 
-        const currDeg = ((currVal - minVal) * 180) / (maxVal - minVal)
-
+        let currDeg = ((currVal - minVal) * 180) 
+        currDeg = ((maxVal - minVal) > 0) ? currDeg / (maxVal - minVal) : 0
+    
         const fontSize = `${currVal}`.length > 4 ? 28 : 38
 
         return (
@@ -29,8 +34,8 @@ export class GaugeCard extends Component {
                 <div className='inner-card-container'>
                 <div className='gauge-container'>
                     <div className='gauge'>
-                        <span className='gauge-min data-val'>{minVal}</span>
-                        <span className='gauge-max data-val'>{maxVal}</span>
+                    <span className='gauge-min data-val'>{minVal}{` `}{unit}</span>
+                        <span className='gauge-max data-val'>{maxVal}{` `}{unit}</span>
                         <div className='gauge-percentage' style={{ transform: `rotate(${currDeg}deg)` }} />
 
                         <span className='gauge-value data-val' style={{
--- vorto-dashboard/src/views/Device/Cards/PercentageCard.jsx
+++ vorto-dashboard/src/views/Device/Cards/PercentageCard.jsx
@@ -1,15 +1,29 @@
 import React, { Component } from 'react'
 
 import { getRepositoryLink } from '../../../util'
+import { isNumber } from 'util'
 
 
 export class PercentageCard extends Component {
   render() {
     const values = this.props.feature.properties
-    const currVal = values.status.value.currentMeasured.value || 0
-    const minVal = 0
-    const maxVal = 100
+    let currVal = 0
+    let minVal = 0
+    let maxVal = 100
+    if(values.status.value.currentMeasured){
+       currVal = (typeof values.status.value.currentMeasured === 'number') ? values.status.value.currentMeasured : 0
+       minVal = (typeof values.status.value.minMeasured === 'number') ? values.status.value.minMeasured : 0
+       maxVal = (typeof values.status.value.maxMeasured === 'number') ? values.status.value.maxMeasured : 100
 
+
+    }
+    if(values.status.value.currentMeasured && values.status.value.currentMeasured.value){
+       currVal =  (typeof values.status.value.currentMeasured.value === 'number') ? values.status.value.currentMeasured.value : currVal
+       minVal = (typeof values.status.value.minMeasured.value === 'number') ? values.status.value.minMeasured.value : 0
+       maxVal = (typeof values.status.value.maxMeasured.value === 'number') ? values.status.value.maxMeasured.value : 100
+    }
+
+  
     const currPerc = ((currVal - minVal) * 100) / (maxVal - minVal)
 
     return (
