dcec53dc11c39942bbec1990642a9b35a461ed68
Tim Tenckhoff <tmtnckff@icloud.com>
2019-10-29 20:10:42 +0800

    added grouped message output in monitoring console, added output of used output plugin, restructuring
    Signed-off-by: Tim Tenckhoff <tmtnckff@icloud.com>

--- vorto-middleware/frontend/e2e/src/app.e2e-spec.ts
+++ vorto-middleware/frontend/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('vorto-middleware-ui app is running!');
+    expect(page.getTitleText()).toEqual('vorto-middleware app is running!');
   });
 
   afterEach(async () => {
