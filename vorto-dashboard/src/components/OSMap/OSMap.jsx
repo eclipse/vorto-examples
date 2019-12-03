4c05eea89950f57f150d4f2a409452935ce87b2c
Tim Tenckhoff <tmtnckff@icloud.com>
2019-12-03 17:48:07 +0800

    fixed bug with map not showing due to false check for geolocation definition
    Signed-off-by: Tim Tenckhoff <tmtnckff@icloud.com>

--- vorto-dashboard/src/components/OSMap/OSMap.jsx
+++ vorto-dashboard/src/components/OSMap/OSMap.jsx
@@ -3,18 +3,15 @@ import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 import log from 'loglevel'
 import DeviceTooltip from '../DeviceTooltip/DeviceTooltip'
 import { store } from '../../store'
-import { getThingsInTopology } from '../../util'
+import { getThingsInTopology, getTextAfterColon } from '../../util'
+import { CATEGORIES } from "../../util/cardUtils"
+
 
 
 log.setLevel(process.env.REACT_APP_LOG_LEVEL || 'debug')
 
 
 class OSMap extends Component {
-  // handle click on marker
-  handleMarkerClick = deviceId => e => {
-    log.debug("marker clicked", { deviceId })
-  };
-
   state = {
     selectedStates: []
   }
@@ -51,61 +48,68 @@ class OSMap extends Component {
     const topology = (store.getState().topologyState) ? store.getState().topologyState : {}
     var selectedDevice = (store.getState().selectedDevice.thingId) ? store.getState().selectedDevice.thingId : ''
     const thingsInTopology = getThingsInTopology(topology, selectedDevice)
+
     const geoLocationDevices = things.map((device, index) => {
 
-      // contains location or geolocation?
-      if (this.isInTopology(device.thingId, thingsInTopology) &&
-        (device.features.location || device.features.geolocation)) {
+      const features = device.features
+      const deviceId = device.thingId
 
-        const deviceLocStatus = (device.features.location !== undefined) ?
-          device.features.location.properties.status : device.features.geolocation.properties.status.geoposition
-        if(deviceLocStatus){
-        if(deviceLocStatus.latitude && deviceLocStatus.longitude){
-          position = [parseFloat(deviceLocStatus.latitude), parseFloat(deviceLocStatus.longitude) ]
+      //check only for all items in the selected topology 
+      if (thingsInTopology.includes(getTextAfterColon(deviceId))) {
+
+        for (var feature in features) {
+          const featureObj = features[feature]
+          let definition = featureObj.definition
+          if (definition[0]) {
+            if (CATEGORIES.LOCATION.includes(definition[0])) {
+              const latitude = featureObj.properties.status.latitude
+              const longitude = featureObj.properties.status.longitude
+
+              if (latitude, longitude) {
+
+                position = [parseFloat(latitude), parseFloat(longitude)]
+                positionGroup.push(position)
+
+                const popUp = this.props.displayTooltip
+                  ? (<Popup>
+                    <DeviceTooltip
+                      device={device}
+                      latitude={latitude}
+                      longitude={longitude}
+                    />
+                  </Popup>)
+                  : <div></div>
+
+                return (
+                  <Marker position={position}
+                    value={deviceId}
+                    key={index} >
+                    {popUp}
+                  </Marker>
+                )
+              }
+            }
+          }
         }
       }
-        const deviceId = device.thingId
-        positionGroup.push(position)
-        if(positionGroup.length === 1){
-          //if only one entry add another bound close to the one
-          positionGroup.push([positionGroup[0][0]+ 0.005 , positionGroup[0][1]+ 0.00001 ])
-        }
 
-        const popUp = this.props.displayTooltip
-          ? (<Popup>
-            <DeviceTooltip
-              device={device}
-            />
-          </Popup>)
-          : <div></div>
-
-        return (
-          <Marker position={position}
-            value={deviceId}
-            onClick={this.handleMarkerClick(deviceId)}
-            key={index} >
-            {popUp}
-          </Marker>
-        )
-      }
-    
-    })
-   
+    }
+    )
 
     if (position.length !== 0 && positionGroup.length > 0) {
       return (<Map className='map-wrapper'
         bounds={[positionGroup]}
-        boundsOptions={{padding: [150, 150]}}
+        boundsOptions={{ padding: [150, 150] }}
         maxZoom={15}
-        >
-       <TileLayer
-            url='http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
-          />
-        <div className="device-counter">Located Devices: {positionGroup.length - 1}</div>
+      >
+        <TileLayer
+          url='http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+        />
+        <div className="device-counter">Located Devices: {positionGroup.length}</div>
         {geoLocationDevices}
       </Map>)
     }
-    
+
     return null
   }
 }
