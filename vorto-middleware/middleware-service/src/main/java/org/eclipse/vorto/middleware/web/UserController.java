84fe20beecc8b6184839bb1eb0f7e38e15ab2da5
Alexander Edelmann <alexander.edelmann@bosch-si.com>
2019-12-11 14:53:00 +0800

    fixed request mapping for user

--- vorto-middleware/middleware-service/src/main/java/org/eclipse/vorto/middleware/web/UserController.java
+++ vorto-middleware/middleware-service/src/main/java/org/eclipse/vorto/middleware/web/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/api/v1/context")
 public class UserController {
 
 	 @RequestMapping(value = {"/user"}, method = RequestMethod.GET)
